7a1fabe488d0de1febdd590e2d4ed071522e4aa8
marades <jkl9510357@gmail.com>
2021-08-26 01:19:06 +0900

    Setting: Swagger 초기 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('내일의 집 토이 프로젝트')
+    .setDescription('내일의 집 토이 프로젝트 API Swagger')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 
   if (module.hot) {
