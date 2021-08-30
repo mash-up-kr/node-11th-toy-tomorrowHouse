7415565d6b5ab1367192df461cee844c57215043
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-08-31 08:35:16 +0900

    Chore: nest-morgan 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MorganInterceptor, MorganModule } from 'nest-morgan';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MorganModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+    provide: APP_INTERCEPTOR,
+    useClass: MorganInterceptor('combined'),
+  }],
 })
 export class AppModule {}
