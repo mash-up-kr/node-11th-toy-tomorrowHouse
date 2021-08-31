5641f61956ea706e8731918e4ed3fe49db54edc5
alwayslee <ddoddo1997@naver.com>
2021-08-31 20:39:34 +0900

    Feat: 회원가입 API에 스웨거 코드 추가

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { User } from '../../entities/user.entity';
 
 @Controller('user')
+@ApiTags('User API')
 export class UserController {
     constructor(private readonly userService: UserService){}
 
     @Post("/register")
+    @ApiOperation({summary: 'Create User API', description:'Create User'})
+    @ApiCreatedResponse({description:'Create User', type: User})
     register(@Body() userData: CreateUserDto){
         return this.userService.register(userData);
     }
