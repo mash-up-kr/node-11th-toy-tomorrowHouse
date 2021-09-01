9ed8c4939697b827d9804f556382417d6e8ae41b
alwayslee <ddoddo1997@naver.com>
2021-09-02 01:19:51 +0900

    Feat: 유저 전시이름 API 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { MorganInterceptor, MorganModule } from 'nest-morgan';
+
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
 
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,18 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Patch, Post } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { User } from '../../entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 @ApiTags('User API')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('/register')
+  @Post()
   @ApiOperation({ summary: 'Create User API', description: 'Create User' })
   @ApiCreatedResponse({ description: 'Create User', type: User })
   register(@Body() userData: CreateUserDto) {
     return this.userService.register(userData);
+  }
+
+  @Patch()
+  @ApiOperation({ summary: 'Update User API', description: 'Update displayed name of user' })
+  @ApiCreatedResponse({ description: 'Update displayed name of user', type: User })
+  updateDisplayedName(@Body() updateUserData: UpdateUserDto){
+    return this.userService.updateDisplayedNameByEmail(updateUserData.email, updateUserData.displayed_name);
   }
 }
