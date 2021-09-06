a06f39b6620fc643486c5ee06d26b35ed0c2a1b3
marades <jkl9510357@gmail.com>
2021-09-06 19:55:16 +0900

    Feat: 채널 수정기능 추가

--- src/modules/channel/channel.controller.ts
+++ src/modules/channel/channel.controller.ts
@@ -1,17 +1,28 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ChannelService } from './channel.service';
-import { createChannelDto } from './dto/create-channel.dto';
+import { CreateChannelDto } from './dto/create-channel.dto';
+import { UpdateChannelDto } from './dto/update-channel.dto';
 
 @ApiTags('Channels API')
 @Controller('channel')
 export class ChannelController {
-  constructor(private readonly channelService: ChannelService) {}
+  constructor(private readonly channelService: ChannelService) {
+    console.log(CreateChannelDto);
+  }
 
   @ApiResponse({ description: '채널 생성 성공' })
   @ApiOperation({ summary: '채널 생성' })
   @Post()
-  async createChannel(@Body() channelData: createChannelDto) {
+  async createChannel(@Body() channelData: CreateChannelDto) {
     const channelId = await this.channelService.createChannel(channelData);
     return { channelId };
   }
@@ -21,5 +32,15 @@ export class ChannelController {
   async getAllChannels(@Query('workspaceId') workspaceId: number) {
     const channels = await this.channelService.getAllChannels(workspaceId);
     return { list: channels };
+  }
+
+  @ApiParam({ name: 'channelId' })
+  @ApiOperation({ summary: '채널 정보 수정' })
+  @Patch('/:channelId')
+  async updateChannel(
+    @Param('channelId') channelId,
+    @Body() channelData: UpdateChannelDto,
+  ) {
+    await this.channelService.updateChannel(channelId, channelData);
   }
 }
--- src/modules/channel/channel.service.ts
+++ src/modules/channel/channel.service.ts
@@ -2,20 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Channel } from 'src/entities/channel.entity';
 import { Repository } from 'typeorm';
+import { CreateChannelDto } from './dto/create-channel.dto';
+import { UpdateChannelDto } from './dto/update-channel.dto';
 
 @Injectable()
 export class ChannelService {
   constructor(
-    @InjectRepository(Channel) private channelRepository: Repository<Channel>,
+    @InjectRepository(Channel)
+    private channelRepository: Repository<Channel>,
   ) {}
 
-  async createChannel(channelData): Promise<number> {
+  async createChannel(channelData: CreateChannelDto): Promise<number> {
     const channel = await this.channelRepository.save(channelData);
     return channel.id;
   }
 
-  async getAllChannels(workspaceId): Promise<Array<Channel>> {
+  async getAllChannels(workspaceId: number): Promise<Array<Channel>> {
     const channels = await this.channelRepository.find({ workspaceId });
     return channels;
+  }
+
+  async updateChannel(id: number, updateData: UpdateChannelDto): Promise<void> {
+    await this.channelRepository.update(id, updateData);
   }
 }
