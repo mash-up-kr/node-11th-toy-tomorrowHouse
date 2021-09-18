1c3e4b2e0eb62a7960bb5dc5db17246fad02eb6e
god9599 <59276134+god9599@users.noreply.github.com>
2021-09-19 01:55:32 +0900

    Feat: 프로필 컬럼들 스웨거 정보 추가

--- src/entities/profile.entity.ts
+++ src/entities/profile.entity.ts
@@ -1,23 +1,48 @@
-import { IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Profile {
+  @ApiProperty({
+    example: 1,
+    description: '프로필 아이디',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: '멋쟁이 개발자',
+    description: '다른 사람들에게 보여주는 이름. 닉네임 같은거~',
+  })
   @Column()
+  @IsString()
   displayName: string;
 
+  // 이거는 임시적인 거임 s3에 올라간 url
+  @ApiProperty({
+    description: '이미지 url',
+  })
   @Column()
+  @IsString()
   @IsOptional()
   imageUrl: string;
 
+  @ApiProperty({
+    example: '개발자',
+    description: '속한 워크스페이스에서의 역할',
+  })
   @Column()
+  @IsString()
   @IsOptional()
   role: string;
 
+  @ApiProperty({
+    example: '010-0000-0000',
+    description: '폰 번호',
+  })
   @Column()
+  @IsString()
   @IsOptional()
   phoneNumber: string;
 }
