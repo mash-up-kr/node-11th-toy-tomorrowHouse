1176a1a985e14a28db7c2cf4d707daf33d12aa97
god9599 <59276134+god9599@users.noreply.github.com>
2021-09-21 01:42:15 +0900

    Feat: 프로필 API 설명 추가

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -10,11 +10,17 @@ import {
 import { ProfileService } from './profile.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { ApiTags, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+import { Profile } from 'src/entities/profile.entity';
 
-@Controller('')
+@ApiTags('Profile API')
+@Controller()
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
+  @ApiOperation({ summary: '프로필 만들기' })
+  @ApiParam({ name: 'userId' })
+  @ApiResponse({ type: Profile, description: '프로필 만들었어용' })
   @Post('user/:userid/profile')
   createProfile(
     @Param('userid') id: number,
@@ -23,11 +29,13 @@ export class ProfileController {
     return this.profileService.createProfile(id, createProfileDto);
   }
 
+  @ApiOperation({ summary: '프로필 정보 가져오기' })
   @Get('profile/:profileid')
   findProfile(@Param('profileid') id: string) {
     return this.profileService.findOneById(+id);
   }
 
+  @ApiOperation({ summary: '프로필 정보 수정하기' })
   @Patch('profile/:profileid')
   updateProfile(
     @Param('profileid') id: string,
@@ -36,6 +44,7 @@ export class ProfileController {
     return this.profileService.updateProfileById(+id, updateProfileDto);
   }
 
+  @ApiOperation({ summary: '프로필 정보 삭제하기' })
   @Delete('profile/:profileid')
   removeProfile(@Param('profileid') id: string) {
     return this.profileService.removeProfileById(+id);
